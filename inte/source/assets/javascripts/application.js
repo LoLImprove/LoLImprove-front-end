a944e58e1ffcf0b852b1a600d11cc6fbe5d45044
Gabriel Dehan <dehan.gabriel@gmail.com>
2016-03-11 15:00:44 +0100

    Moved out

---
+++ inte/source/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//= require_tree .
+
+$(document).ready(function() {
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+});
